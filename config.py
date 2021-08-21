671c77d1df8907d3f3f0d16f5659aec85172e4da
aveBHS <avebhs.ru@gmail.com>
2021-08-22 00:04:30 +0300

    Create config file

---
+++ config.py
@@ -0,0 +1,29 @@
+configs = {
+    "debug": True,
+    "credentials": {
+        "api_key": "HpwpTEwJdrCHNVUnF3Ppv_uqVVkA5vuJ",
+        "mysql": {
+            "host": "localhost",
+            "user": "root",
+            "password": "root",
+            "base": "inpars"
+        }
+    },
+    "regions": [77]
+}
+
+
+def config(param):
+    try:
+        value = None
+        for route in param.split("."):
+            if route in configs.keys() or value and route in value.keys():
+                if value is not None:
+                    value = value[route]
+                else:
+                    value = configs[route]
+        if value:
+            return value
+        return None
+    except TypeError:
+        return None
